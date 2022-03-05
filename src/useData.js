638188fdd9bfe5f1dc0e1c85d8f2d05651621d9f
Tolga Bakır <50793243+Sefirott@users.noreply.github.com>
2022-03-05 23:16:54 +0300

    Update useData.js

--- src/useData.js
+++ src/useData.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { csv } from "https://cdn.skypack.dev/d3";
+import * as d3 from "d3";
 
 const csvUrl = "https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv";
 
@@ -11,7 +11,7 @@ useEffect(() => {
         d.Population = +d["2020"] * 1000;
         return d;
     }
-    csv(csvUrl, row).then(data => {
+    d3.csv(csvUrl, row).then(data => {
         setData(data.slice(0, 10));
     });
 }, [])
